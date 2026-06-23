e1f680cfc845855d344ed9e504601cd57e984ddf
Douglas Souza <douglassouzaoficiall@gmail.com>
2026-06-23 13:33:16 -0300

    feat: reduz font-size do titulo da hero para 55px e melhora quebra de linha

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,9 +7,8 @@ const Hero: React.FC = () => {
       <div className="container hero-container">
         <div className="hero-content animate-fade-left">
           <h1 className="hero-title">
-            16 anos transformando<br />
-            energia em desenvolvimento<br />
-            sustentável.
+            16 anos transformando energia<br />
+            em desenvolvimento sustentável.
           </h1>
           <p className="hero-desc">
             Projetamos, executamos e gerenciamos iniciativas de eficiência energética, educação, sustentabilidade, desenvolvimento social e impacto comunitário para concessionárias de energia e instituições em todo o Brasil.
